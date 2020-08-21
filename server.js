efb9ca1b9c46fceb90d87a576bb0e1b309b70b5c
jgnovais <jgnx@hotmail.com>
2020-08-21 00:00:29 -0300

    make request to maps with the express js

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const request = require('request')
+const serveStatic = require('serve-static')
+const app = express()
+app.use(serveStatic(__dirname + "/dist"));
+const port = 3000
+
+app.get('/', async (req, res) => {
+    await request('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-8.119213199999999,-35.093855&radius=5000&language=pt-BR&key='+ process.env.GOOGLE_KEY,
+     { json: true }, function(error, response, body) {
+        res.json(body)
+      })
+})
+
+app.listen(port, () => {
+
+  console.log(`Example app listening at http://localhost:${port}`)
+})
