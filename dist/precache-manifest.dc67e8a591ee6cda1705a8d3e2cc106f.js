14c6de7117246a2eeb35f3f87cd3ec506bd448be
jgnovais <jgnx@hotmail.com>
2020-08-21 00:39:11 -0300

    Adding files.

---
+++ dist/precache-manifest.dc67e8a591ee6cda1705a8d3e2cc106f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c8d2c0795dc4b378ac3",
+    "url": "/css/app.d40fc157.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "bc4087c4421acfb2ebee45c4059212b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c8d2c0795dc4b378ac3",
+    "url": "/js/app.9243a514.js"
+  },
+  {
+    "revision": "08e6947726b28ad52866",
+    "url": "/js/chunk-vendors.256dc71b.js"
+  },
+  {
+    "revision": "09646f54a42da361ac95b52b66731d0c",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
