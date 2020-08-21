9c6d2832d9fa3ead2121a9d1a567a62537d0064b
jgnovais <jgnx@hotmail.com>
2020-08-21 00:34:10 -0300

    heroku

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express')
 const request = require('request')
 const serveStatic = require('serve-static')
 const app = express()
-app.use(serveStatic(__dirname + "/dist"));
-const port = 80
+app.use(serveStatic(__dirname + "/dist"))
+const port = process.env.PORT || 5000
 
 app.get('/nearbysearch', async (req, res) => {
     await request('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-8.119213199999999,-35.093855&radius=5000&language=pt-BR&key='+ process.env.GOOGLE_KEY,
